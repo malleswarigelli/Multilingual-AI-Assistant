afbcc407ce0b35e4d854f1f382efcad8f4afe295
Malli Gelli <malleswari.gelli@gmail.com>
2024-04-10 11:51:56 -0500

    app.py added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.helper import voice_input, llm_model_object, text_to_speech
 
 
 def main():
-    st.title("Multilingual AI Assistant")
+    st.title("Multilingual AI Assistant 🤖")
     
     # creating a button
     if st.button("Ask me anything"):
